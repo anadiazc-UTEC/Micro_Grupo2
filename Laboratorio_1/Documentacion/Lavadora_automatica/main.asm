965eff7f51774af9beb156bfbe4a6740b83393cc
Matheo Santana <matheo.santana@estudiantes.utec.edu.uy>
2026-08-18 20:15:26 -0300

    primera iteracion de lavadora, primeras pruebas de maquina de estados en ensamblador

---
+++ Laboratorio_1/Documentacion/Lavadora_automatica/main.asm
@@ -0,0 +1,55 @@
+
+;Estados: 
+;0x00: Espera
+;0x01: Lavado
+;0x02: Centrifugado
+;0x03: Secado
+;0x04: Finalizar
+
+.include "m328pdef.inc"
+.org 0x00
+
+ldi r16, 0x00
+
+start:
+	
+    cpi r16, 0x00
+	breq espera
+
+	cpi r16, 0x01
+	breq lavado
+
+    cpi r16, 0x02
+	breq centrifugado
+
+	cpi r16, 0x03
+	breq secado
+
+	cpi r16, 0x04
+	breq finalizar
+
+	ldi r16, 0x00
+	rjmp start
+
+espera:
+	ldi r16, 0x01
+	rjmp fin_if
+
+lavado:
+	ldi r16, 0x02
+	rjmp fin_if
+
+centrifugado:
+	ldi r16, 0x03
+	rjmp fin_if
+
+secado:
+	ldi r16, 0x04
+	rjmp fin_if
+
+finalizar:
+	ldi r16, 0x00
+	rjmp fin_if
+
+fin_if:
+	rjmp start
