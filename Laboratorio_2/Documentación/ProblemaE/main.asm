b53f89fbc937476611654ca6ccba7aeab7b112e6
Matheo Santana <matheo.santana@estudiantes.utec.edu.uy>
2026-09-25 16:21:16 -0300

    msantana estados

--- Laboratorio_2/Documentación/ProblemaE/main.asm
+++ Laboratorio_2/Documentación/ProblemaE/main.asm
@@ -1,7 +1,14 @@
 .include "m328pdef.inc"
 
-.def temp = r16
-.def estado = r17
+.def temp	= r16
+.def estado	= r17
+
+;CERRADA	= 0x00
+;ABRIENDO	= 0x01
+;ABIERTA	= 0x02
+;CERRANDO	= 0x03
+;BLOQUEADA	= 0x04
+
 .org 0x0000
 	rjmp RESET
 .org 0x000A		;PCINT2 (PORTD)
@@ -38,5 +45,120 @@ RESET:
 	ldi temp,	(1<<PCINT22)
 	sts PCMSK2,	temp
 
-start:
-    rjmp start
+	ldi estado, 0x00
+
+main_loop:
+	
+	cpi estado, 0x00
+	breq ejecutar_cerrada
+	
+	cpi estado, 0x01
+	breq ejecutar_abriendo
+	
+	cpi estado, 0x02
+	breq ejecutar_abierta
+	
+	cpi estado, 0x03
+	breq ejecutar_cerrando
+	
+	cpi estado, 0x04
+	breq ejecutar_bloqueada
+	
+	rjmp main_loop
+	
+ejecutar_cerrada:
+
+	; apagar todo
+	ldi temp, 0x00	
+	out PORTB, temp
+	
+	; boton abrir
+	sbic PIND, PIND2
+	rjmp boton_abrir
+	
+	rjmp main_loop
+
+ejecutar_abriendo:
+
+	;abrir puerta y sonar alarma
+	ldi temp, (1<<PINB0) | (1<<PINB2) 
+	out PORTB, temp
+	
+	;sensor abierto
+	sbic PIND, PIND4
+	rjmp sensor_abierto
+	
+	rjmp main_loop
+
+ejecutar_abierta:
+
+	; apagar todo
+	ldi temp, 0x00
+	out PORTB, temp
+	
+	;boton cerrar
+	sbic PIND, PIND3
+	rjmp boton_cerrar
+	
+	rjmp main_loop
+
+ejecutar_cerrando:
+
+	;cerrar puerta y sonar alarma
+	ldi temp, (1<<PINB1) | (1<<PINB2)
+	out PORTB, temp
+	
+	;sensor cerrado
+	sbic PIND, PIND5
+	rjmp sensor_cerrado
+
+	rjmp main_loop
+
+ejecutar_bloqueada:
+	; apagar todo
+	ldi temp, 0x00
+	out PORTB, temp
+	
+	; boton abrir
+	sbic PIND, PIND2
+	rjmp boton_abrir
+	
+	; boton cerrar
+	sbic PIND, PIND3
+	rjmp boton_cerrar
+	
+	rjmp main_loop
+
+boton_abrir:
+	ldi estado, 0x01
+	rcall delay_12ms
+	rjmp main_loop
+
+boton_cerrar:
+	ldi estado, 0x03
+	rcall delay_12ms
+	rjmp main_loop
+
+sensor_abierto:
+	ldi estado, 0x02
+	rcall delay_12ms
+	rjmp main_loop
+
+sensor_abierto:
+	ldi estado, 0x00
+	rcall delay_12ms
+	rjmp main_loop
+
+delay_12ms:
+	ldi r28, 255
+l2:
+	ldi r29, 250
+l1:
+	
+	dec r29
+	brne l1
+
+	dec r28
+	brne l2	
+	
+	ret
